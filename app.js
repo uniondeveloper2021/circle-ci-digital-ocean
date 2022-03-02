b0c867ccc517316bab01b4686642feb258d8a2b4
uniondeveloper2021 <86847362+uniondeveloper2021@users.noreply.github.com>
2022-03-02 12:29:08 -0500

    modify app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', function (req, res) {
-    res.json({ message: 'message from vsc123' });
+    res.json({ message: 'message from vsc soly 123' });
 });
 
 app.get('/message-test', function (req, res) {
