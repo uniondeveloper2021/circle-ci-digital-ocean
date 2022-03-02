3e3899dfc4ba961693f91fd7b7801d7b90faf999
uniondeveloper2021 <86847362+uniondeveloper2021@users.noreply.github.com>
2022-03-02 11:52:20 -0500

    add express and jest and update config.yml

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const http = require('http');
+var express = require("express");
+var app = express();
 
-http.createServer(function (req, res) {
-  res.write('Hello World again12!'); 
-  res.end(); 
-}).listen(4000, '0.0.0.0'); 
+app.listen(4000, () => console.log('running on 4000'));
+const cors = require("cors");
+
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.json({ message: 'message' });
+});
+
+app.get('/message-test', function (req, res) {
+    res.json({ message: 'message-test' });
+});
