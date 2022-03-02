0d4b0df83ebfed49142f9d4d60987bd8c6028a4b
uniondeveloper2021 <86847362+uniondeveloper2021@users.noreply.github.com>
2022-03-02 11:03:24 -0500

    add json to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.status(200).json({ status: true, resp: 'ok', message: 'Hello World' });
-}).listen(3009, '0.0.0.0'); 
+  res.write('Hello World!'); 
+  res.end(); 
+}).listen(3000, '0.0.0.0'); 
